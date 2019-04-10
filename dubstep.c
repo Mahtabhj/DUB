f6d897d626905d4b02dcb2d260cd5e278a73ea40
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2019-04-10 12:19:47 +0600

    Update dubstep.c

--- dubstep.c
+++ dubstep.c
@@ -1,30 +1,41 @@
 #include<stdio.h>
 #include<string.h>
+int pal(char s[1005],int o, int l)
+{
+    int i,j,c=0;
+    for(i=o,j=l;i<=l+1/2;i++,j--)
+    {
+        if(s[i]==s[j])
+        {
+            continue;
+        }
+        else {return 0;}
+    }
+    return 1;
+}
 int main()
 {
-    char s[200];
-
-    int i,n,j,c=0,p;
-    scanf("%d %d",&p,&n);scanf("%s",&s);
-    int l=strlen(s);
-    for(i=1;i<=n;i++)
+    char s[1005];
+    scanf("%s",&s);
+    int n,i,l,k,d,c=0,m,j=0;l=strlen(s);
+    scanf("%d",&k);
+    for(i=1;i<l;i++)
     {
-
-        for(j=0;j<l-1;j++)
+        if(pal(s,j,i)==1)
         {
-            if(s[j]!=s[j+1])
-            {
-                char g;
-                g=s[j];
-                s[j]=s[j+1];
-                s[j+1]=g;
-                j=j+1;
-            }
-            else {continue ;}
+            c=c+1;m=i;d=i-j;
+            j=i+1;i=j+d-1;
         }
-
+        else if(j>=l-1){break ;}
     }
-    printf("%s",s);
-    return 0;
+    if(c==k&&m==l-1)
+    {
+        printf("YES");
+    }
+    else if(l==1)
+    {
+        printf("YES");
+    }
+    else {printf("NO");}
 }
 
